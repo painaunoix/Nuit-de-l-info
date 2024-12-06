ae987871b7e35940f31cd43628d70e7470cca7f0
Gabriel Leveque. <gabriel.leveque@epitech.eu>
2024-12-06 01:51:58 +0100

    [+][background] background images seté

---
+++ src/app/components/sea/sea.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sea',
+  imports: [],
+  templateUrl: './sea.component.html',
+  styleUrl: './sea.component.css'
+})
+export class SeaComponent {
+
+}
