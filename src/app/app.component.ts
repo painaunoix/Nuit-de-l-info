8ea762ee78c6aa063c0b9540d0fa6d676e066d9b
Ewan <ewan.schaffhauser@gmail.com>
2024-12-06 07:07:29 +0100

    [+] change lyreco et add starting of the quest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,13 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { GameComponent } from './components/trash-slider/trash-slider.component';
 import { LyrecoComponent } from './components/lyreco/lyreco.component';
 import { SeaComponent } from './components/sea/sea.component';
+import { QuestComponent } from './components/quest/quest.component'
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, SeaComponent, LyrecoComponent],
+  imports: [RouterOutlet, NavbarComponent, SeaComponent, LyrecoComponent, QuestComponent, MatDialogModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/sea/sea.component.ts
+++ src/app/components/sea/sea.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { LyrecoComponent } from '../lyreco/lyreco.component';
 import { GameComponent } from '../trash-slider/trash-slider.component';
-
+import { QuestComponent } from '../quest/quest.component';
 @Component({
   selector: 'app-sea',
-  imports: [GameComponent, LyrecoComponent],
+  imports: [GameComponent, LyrecoComponent, QuestComponent],
   templateUrl: './sea.component.html',
   styleUrl: './sea.component.css'
 })
