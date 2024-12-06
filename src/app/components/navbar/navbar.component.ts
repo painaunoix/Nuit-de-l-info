f7c5062b458b8443a07ae516f42509a42e36743c
Hedi2091 <146425721+Hedi2091@users.noreply.github.com>
2024-12-06 05:12:44 +0100

    BADGAB

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { Router, RouterLink, Routes } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { ChangeVueService } from '../../service/change-vue.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  constructor(private ChangeVueService: ChangeVueService) {}
+    constructor(private ChangeVueService: ChangeVueService,
+                private router: RouterModule    ) { }
 
   onScrollToBottom() {
       this.ChangeVueService.scrollToBottom(2);
