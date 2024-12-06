29a4affe26df336ff582a94ca579e24a7ae32619
Gabriel Leveque. <gabriel.leveque@epitech.eu>
2024-12-06 04:50:15 +0100

    [+][body] oui

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-body',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
-
+  torso_clicked = false;
+  head_clicked = false;
 }
